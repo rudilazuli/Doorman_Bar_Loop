2ccb671feb009b3e36dac9c0bb893af748f48e5d
rudilazuli <100395138+rudilazuli@users.noreply.github.com>
2022-08-06 17:43:10 -0700

    Create Doorman_Bar_Loop.py

---
+++ Doorman_Bar_Loop.py
@@ -0,0 +1,8 @@
+#Your computer is the doorman at a bar in a country where the drinking age is 21.
+
+ages = [12, 38, 34, 26, 21, 19, 67, 41, 17]
+
+for age in ages:
+  if age < 21:
+    continue
+  print(age)
